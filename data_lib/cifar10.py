405fe77791cdc4beb3aa5e894d4e5856a4add44d
hgarud <hrishi.garud@gmail.com>
2023-12-25 15:43:25 -0800

    add cifar10 dataloader

---
+++ data_lib/cifar10.py
@@ -0,0 +1,21 @@
+"""Cifar10 dataset module."""
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+def get_dataset(root: str, train: bool) -> torch.utils.data.Dataset:
+    # Define the transformation to apply to the dataset
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    # Load the CIFAR-10 training dataset
+    dataset = torchvision.datasets.CIFAR10(root=root,
+                                            train=train,
+                                            transform=transform)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=4,
+                                            shuffle=(train==True), num_workers=2)
+
+    return dataloader
